abc4640f94600aca0fc2996b0add7e704d3b1220
Carlos Alvarez <cafregoso@outlook.com>
2022-05-12 17:34:10 -0500

    styles for info containers

--- src/Components/PagesComponents/Footer/Footer.jsx
+++ src/Components/PagesComponents/Footer/Footer.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <div className='footer-privacy'>
+        
+      </div>
+      <div className='footer-contact'>
+        
+      </div>
+      <div className='footer-newsletter'>
+        
+      </div>
+    </div>
   )
 }
